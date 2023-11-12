752e4f3acb86a69c6b56c79ffa92a5893660e80b
Jamal Kayed <119996570+Yuroq@users.noreply.github.com>
2023-11-12 18:02:56 -0500

    Add files via upload

--- src/Components/Input.jsx
+++ src/Components/Input.jsx
@@ -8,90 +8,93 @@ function InputField(props) {
   const [yearInput, setYearInput] = useState("");
   const setHourInput = props.setHourInput;
   const setInputDate = props.setInputDate;
-  const setMinuteInput = props.setMinuteInput
-  const setAmPm = props.setAmPm
-  const setInvalidInputMessage = props.setInvalidInputMessage
+  const setMinuteInput = props.setMinuteInput;
+  const setAmPm = props.setAmPm;
+  const setInvalidInputMessage = props.setInvalidInputMessage;
   const onClickHandler = () => {
-    if(monthInput == "" || dayInput == "" || yearInput == "") {
-setInvalidInputMessage(true)
+    if (monthInput == "" || dayInput == "" || yearInput == "") {
+      setInvalidInputMessage(true);
     }
     setInputDate(`${monthInput} ${dayInput} ${yearInput}`);
   };
-console.log(monthInput)
+
   return (
     <div className="input-Field-Div">
       <select
         className="select select-info w-full max-w-xs"
         onChange={(input) => setMonthInput(input.target.value)}
       >
-        <option disabled selected>
+        <option disabled value>
           Select Month
         </option>
-        {InputSelectors.months.map((month) => {
-          return <option>{month}</option>;
+        {InputSelectors.months.map((month, index) => {
+          return <option key={index}>{month}</option>;
         })}
       </select>
       <select
         className="select select-info w-full max-w-xs"
         onChange={(input) => setDayInput(input.target.value)}
       >
-        <option disabled selected>
+        <option disabled value>
           Select Day
         </option>
-        {InputSelectors.days.map((day) => {
-          return <option>{day}</option>;
+        {InputSelectors.days.map((day, index) => {
+          return <option key={index}>{day}</option>;
         })}
       </select>
       <select
         className="select select-info w-full max-w-xs"
         onChange={(input) => setYearInput(input.target.value)}
       >
-        <option disabled selected>
+        <option disabled value>
           Select Year
         </option>
-        {InputSelectors.years.map((year) => {
-          return <option>{year}</option>;
+        {InputSelectors.years.map((year, index) => {
+          return <option key={index}>{year}</option>;
         })}
       </select>
       <select
         className="select select-info w-full max-w-xs"
         onChange={(input) => setHourInput(input.target.value)}
       >
-        <option disabled selected>
+        <option disabled value>
           Select Hour
         </option>
-        {InputSelectors.hours.map((hour) => {
-          return <option>{hour}</option>;
+        {InputSelectors.hours.map((hour, index) => {
+          return <option key={index}>{hour}</option>;
         })}
       </select>
-        <select
+      <select
         className="select select-info w-full max-w-xs"
         onChange={(input) => setMinuteInput(input.target.value)}
       >
-        <option disabled selected>
+        <option disabled value>
           Select Minute
         </option>
-        {InputSelectors.minutes.map((minute) => {
-          return <option>{minute}</option>;
+        {InputSelectors.minutes.map((minute, index) => {
+          return <option key={index}>{minute}</option>;
         })}
       </select>
       <div className="AmPM-div">
-      <select
-        className="select select-info w-full max-w-xs"
-        onChange={(input) => setAmPm(input.target.value)}
-      >
-        <option disabled selected>
-          Time
-        </option>
-        {InputSelectors.time.map((time) => {
-          return <option>{time}</option>;
-        })}
-      </select>
+        <select
+          className="select select-info w-full max-w-xs"
+          onChange={(input) => setAmPm(input.target.value)}
+        >
+          <option disabled value>
+            Time
+          </option>
+          {InputSelectors.time.map((time, index) => {
+            return <option key={index}>{time}</option>;
+          })}
+        </select>
       </div>
       <div className="start-btn">
-      <button className="btn btn-active btn-secondary" onClick={onClickHandler}>
-        Start!
-      </button>
+        <button
+          className="btn btn-active btn-secondary"
+          onClick={onClickHandler}
+        >
+          Start!
+        </button>
       </div>
     </div>
   );
